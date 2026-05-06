608a790bacbe204888a0aa22752008a5d4105de0
Mohamed El Fatine <simohaji147@gmail.com>
2026-05-06 18:35:56 +0200

    edit

--- src/main/java/com/smartenergy/model/Dispositivo.java
+++ src/main/java/com/smartenergy/model/Dispositivo.java
@@ -1,4 +1,19 @@
 package com.smartenergy.model;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class Dispositivo {
+
+    private Long id;
+    private String nombre;
+    private String tipo;
+    private Double potenciaNominalW;
+    private Boolean activo;
+    private List<Lectura> lecturas;
 }
--- src/main/java/com/smartenergy/model/Lectura.java
+++ src/main/java/com/smartenergy/model/Lectura.java
@@ -1,4 +1,17 @@
 package com.smartenergy.model;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class Lectura {
+
+    private Long id;
+    private LocalDate fechaHora;
+    private Double consumoKwh;
+    private Double costeEuros;
+    private Dispositivo dispositivo;
 }
